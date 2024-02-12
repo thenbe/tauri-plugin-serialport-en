ad37e901b5d5c94a63c23d163e284a0753f4d114
Naser Aleisa <aleisa.naser@gmail.com>
2024-02-13 00:04:10 +0300

    autogenerate permissions

--- build.rs
+++ build.rs
@@ -1,4 +1,14 @@
-const COMMANDS: &[&str] = &["close-all"];
+const COMMANDS: &[&str] = &[
+    "available_ports",
+    "cancel_read",
+    "close",
+    "close_all",
+    "force_close",
+    "open",
+    "read",
+    "write",
+    "write_binary",
+];
 
 fn main() {
     tauri_plugin::Builder::new(COMMANDS)
